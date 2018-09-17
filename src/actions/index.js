5a22dd62497ad83e6a3be86856089ff8eb7f286d
rurigrass <ruripg@gmail.com>
2018-09-18 00:54:56 +0100

    finito

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,13 @@
 import axios from 'axios';
 
 const API_KEY = "473161d12ca2dff108d5cf25f313d974";
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
-
-    console.log('Request:', request);
-    
-
 
     return {
         type: FETCH_WEATHER,
